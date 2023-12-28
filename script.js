8b99b93adce88432bd2245ea35c8e9f386cbe02a
Chirayu Maru <144110269+chirayu2107@users.noreply.github.com>
2023-12-28 16:22:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var audio = document.getElementById('audioplayer');
+    var playButton = document.getElementById('jokeBtn');
+
+    playButton.addEventListener('click', function () {
+        audio.play();
+    });
+});
+
+ 
